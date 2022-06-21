76a91425d5b700b3d8c2a5785b6cc26d72c3edd9
Ramakant Pawar <ramakantpawar36@gmail.com>
2022-06-21 19:03:39 +0530

    assignment2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [newdata, setNewdata] = useState({
+    firstname: '',
+    lastname: ''
+  });
+  const addnewdataHandle = (e) => {
+    e.preventDefault();
+
+    const fieldname = e.target.getAttribute('name');
+    const fieldvalue = e.target.value;
+
+    const newFormData = { ...newdata};
+    newFormData[fieldname] = fieldvalue;
+
+    setNewdata(newFormData);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newData = {
+      firstname: newdata.firstname,
+      lastname: newdata.lastname
+    }
+
+    const newDatas = [...data, newData]
+    setData(newDatas);
+  }
+
+console.log()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h2>Add New Data</h2>
+      <form  onSubmit={handleSubmit}>
+      <label>First Name :</label>
+        <input 
+          type="text"
+          name="firstname"
+          placeholder="Enter First Name..."
+          onChange={addnewdataHandle}
+        />
+        <input 
+          type="text"
+          name="lastname"
+          placeholder="Enter First Name..."
+          onChange={addnewdataHandle}
+        />
+        <button type="submit" class="btn btn-primary"  >Add</button>
+      </form>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((c) => (
+                <tr>
+                <td>{c.firstname}</td>
+                <td>{c.lastname}</td>
+              </tr>
+              ))
+            }
+          </tbody>
+        </table>
     </div>
   );
 }
